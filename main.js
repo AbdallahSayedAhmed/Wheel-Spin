fd1e35a2a68b9ba169c3beac814a66f2721527a6
AbdallahSayedAhmed <abdallahboy2014@gmail.com>
2023-11-17 22:15:06 +0200

    efficient shuffle

--- main.js
+++ main.js
@@ -7,8 +7,27 @@ const popup = document.querySelector(".popup");
 const title = document.querySelector(".title");
 const button = document.querySelector(".spinBtn");
 
+// function shuffle(array) {
+//   return array.sort(() => Math.random() - 0.5);
+// }
 function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
 
 function spin() {
